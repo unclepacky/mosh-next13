04d62ddcc27b3f9bd83f29da2d2a89af097d84d9
UnclePacky <claudeaoun@gmail.com>
2023-10-22 23:58:17 +0300

    Product card and addtocart components

---
+++ app/components/AddToCard.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const AddToCard = () => {
+  return (
+    <div>
+      <button onClick={() => console.log("Clicked")}>Add to card</button>
+    </div>
+  );
+};
+
+export default AddToCard;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
+import Link from "next/link";
+import ProductCard from "./components/ProductCard";
 
 export default function Home() {
   return (
     <main>
       <h1>Main Page</h1>
+      <Link href="/users">Users</Link>
+      <ProductCard />
     </main>
   );
 }
